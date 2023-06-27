ce9feb3abcced0b6aede913d8718ecc33e8c8208
CrisVdev <cristina_cris100@hotmail.com>
2023-06-27 11:29:18 +0200

    components

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const getWordFromApi = () => {
+  return fetch('https://dev.adalab.es/api/random/word')
+    .then((response) => response.json())
+    .then((response) => {
+      return response.word;
+    });
+};
+export default getWordFromApi;
